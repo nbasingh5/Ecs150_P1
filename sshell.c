61b0d87f36bf3a209baeca8ee8efb6478be09765
Aman Singh <singh.aman1@yahoo.com>
2019-01-14 21:27:59 -0800

    Add files via upload
    Completed phase 1

--- sshell.c
+++ sshell.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+ #include <sys/wait.h>
+ #include <unistd.h>
 
 int main(int argc, char *argv[])
 {
-	char *cmd = "/bin/date -u";
-	int retval;
+	char *cmd = "/bin/date";
+	char *args[] = { cmd, "-u", NULL };
+	int status;
 
-	retval = system(cmd);
-	fprintf(stdout, "Return status value for '%s': %d\n", cmd, retval);
-
-	return EXIT_SUCCESS;
+	pid_t pid;
+	pid = fork();
+	if (pid > 0) {
+		waitpid(-1, &status, 0);
+		printf("+ completed '/bin/date -u' [%d]\n", WEXITSTATUS(status));
+	}
+	else if (pid == 0) {
+		execv(cmd,args);
+		perror("execve");
+	}
 }
