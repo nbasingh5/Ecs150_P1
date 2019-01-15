9e6f3d9f106fe93f57d7b3106fce1c217270fc4d
Aman Singh <singh.aman1@yahoo.com>
2019-01-14 22:31:31 -0800

    Add files via upload
    partially done, getting permission denied error

--- sshell.c
+++ sshell.c
@@ -1,13 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+ #include <sys/wait.h>
+ #include <unistd.h>
 
 int main(int argc, char *argv[])
 {
-	char *cmd = "/bin/date -u";
-	int retval;
+	char *cmd = "/bin/";
+	char *userInput;
+	size_t bufsize = 32;
+  size_t characters;
+	char *args[] = { cmd, "-u", NULL };
+	int status;
+	pid_t pid;
 
-	retval = system(cmd);
-	fprintf(stdout, "Return status value for '%s': %d\n", cmd, retval);
+	pid = fork();
+	if (pid > 0) {
+		waitpid(-1, &status, 0);
+		fprintf(stderr, "+ completed '/bin/date -u' [%d]\n", WEXITSTATUS(status));
+	}
+	else if (pid == 0) {
+		userInput = (char *)malloc(bufsize * sizeof(char));
+		if( userInput == NULL)
+		 {
+				 perror("Unable to allocate buffer");
+				 exit(1);
+		 }
+		printf("sshell$ ");
+		characters = getline(&userInput,&bufsize,stdin);
 
-	return EXIT_SUCCESS;
+		char *combinedCmd = (char *) malloc(1 + strlen(cmd)+ strlen(userInput) );
+		strcpy(combinedCmd,cmd);
+		strcat(combinedCmd,userInput);
+
+		printf("Line:, %s", combinedCmd);
+		execv(cmd,args);
+		perror("execve");
+	}
+
+
+
+
 }
