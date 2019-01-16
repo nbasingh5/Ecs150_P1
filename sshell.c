25ca98457f4e1478d44ce42df60f264da3ec7b62
Evelyn <evelyn.morales@girlswhocode.com>
2019-01-15 17:59:32 -0800

    phase 2 done

--- sshell.c
+++ sshell.c
@@ -1,23 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
- #include <sys/wait.h>
- #include <unistd.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[])
 {
-	char *cmd = "/bin/date";
-	char *args[] = { cmd, "-u", NULL };
-	int status;
+  char *cmd = "/bin/";
+  char *userInput;
+  size_t bufsize = 32;
+  size_t characters;
+  int status;
+  pid_t pid;
+  char *combinedCmd;
 
-	pid_t pid;
-	pid = fork();
-	if (pid > 0) {
-		waitpid(-1, &status, 0);
-		printf("+ completed '/bin/date -u' [%d]\n", WEXITSTATUS(status));
-	}
-	else if (pid == 0) {
-		execv(cmd,args);
-		perror("execve");
-	}
+  userInput = (char *)malloc(bufsize * sizeof(char));
+  if( userInput == NULL)
+  {
+    perror("Unable to allocate buffer");
+    exit(1);
+  }
+  printf("sshell$ ");
+  characters = getline(&userInput,&bufsize,stdin);
+  //printf("UserInput: [%s]", userInput);
+  for (int i = 0; i < strlen(userInput); i++)
+  {
+    if ( userInput[i] == '\n' || userInput[i] == '\r' )
+      userInput[i] = '\0';
+  }
+  //printf("NEW UserInput: [%s]", userInput);
+
+  combinedCmd = (char *) malloc(1 + strlen(cmd)+ strlen(userInput) );
+  strcpy(combinedCmd,cmd);
+  strcat(combinedCmd,userInput);
+  char *args[] = {combinedCmd, "-u", NULL };
+  //printf("Line: [%s]", args[0]);
+
+  pid = fork();
+  if (pid > 0) {
+    waitpid(-1, &status, 0);
+    fprintf(stderr, "+ completed '/bin/date -u' [%d]\n", WEXITSTATUS(status));
+  }
+  else if (pid == 0) {
+
+    execvp(combinedCmd,args);
+    perror("execvp");
+  }
 }
